853f1d0929193e353610b5247b2068f6df91c7e9
BezborodovVV37 <blood371993@gmail.com>
2024-08-04 14:00:17 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,15 +1,15 @@
 my_dict={'Василий': 1993, 'Алина': 1998}
 print(my_dict)
-my_dict['Василий']=1991
-my_dict['Иван']=1990
+my_dict.update({'Игорь':2001, 'Катя': 2003})
 print(my_dict)
 my_dict={'Папа':1972,'Мама':1973}
 del my_dict['Папа']
 print(my_dict)
-my_set=(9,8,7,6,9,8,7,6)
-my_set= set(my_set)
+my_set={9,8,7,6,9,8,7,6, (1,2,3,4,),"Привет"}
+print (my_set)
+my_set.update({32,61,42})
 print(my_set)
-print(my_set.discard(7))
+print(my_set.discard(42))
 print(my_set)
-print(my_set.remove(6))
-print(my_set)
+
+
